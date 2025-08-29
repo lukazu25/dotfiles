1619c04640007db77a30b0f443137578e6a7f303
Luka Zukhbaia <145780979+lukazu25@users.noreply.github.com>
2025-08-29 13:03:29 +0400

    Update init.el

--- EMACS/init.el
+++ EMACS/init.el
@@ -49,6 +49,13 @@
   :custom
   (all-the-icons-dired-monochrome nil)) ;; Set to t for monochrome icons
 
+;; Add icons to minibuffer using all-the-icons-completion
+(use-package all-the-icons-completion
+  :after (marginalia all-the-icons)
+  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
+  :init
+  (all-the-icons-completion-mode))
+
 ;; Theme (Doom)
 (use-package doom-themes
   :config
