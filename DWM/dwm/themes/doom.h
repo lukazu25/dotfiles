bf130c68635dd341b1962a95f4896915eeea9577
Luka Zukhbaia <145780979+lukazu25@users.noreply.github.com>
2025-09-19 15:44:34 +0400

    Update doom.h

--- DWM/dwm/themes/doom.h
+++ DWM/dwm/themes/doom.h
@@ -1,14 +1,13 @@
-static const char col_gray1[]   = "#282c34"; // bg (DoomOne background)
-static const char col_gray2[]   = "#1c1f24"; // color01 (DoomOne darker color)
-static const char col_gray3[]   = "#bbc2cf"; // fg (DoomOne foreground)
-static const char col_gray4[]   = "#282c34"; // bg (DoomOne background)
-static const char col_accent[]  = "#46d9ff"; // color02 (DoomOne accent color)
+static const char col_gray1[]   = "#282c34"; 
+static const char col_gray2[]   = "#1c1f24"; 
+static const char col_gray3[]   = "#bbc2cf"; 
+static const char col_gray4[]   = "#282c34"; 
+static const char col_accent[]  = "#46d9ff"; 
 
-/* SchemeNorm uses background and foreground colors for normal state */
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-    [SchemeSel]  = { col_gray4, col_accent, col_accent },
+    [SchemeSel]  = { col_accent, col_gray4, col_accent },
     [SchemeHid]  = { col_accent, col_gray1, col_accent },
 };
 
