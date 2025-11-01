8125d0c93698c6f748586b57f7140bd2326f013d
lukazu25 <145780979+lukazu25@users.noreply.github.com>
2025-11-01 13:07:57 +0400

    Update tokyonight.h

--- DWM/dwm/themes/tokyonight.h
+++ DWM/dwm/themes/tokyonight.h
@@ -4,6 +4,7 @@ static const char col_gray2[] = "#49415f";
 static const char col_gray3[] = "#c0c0c0";
 static const char col_gray4[] = "#f0f0f0";
 static const char col_accent[] = "#a0a0aa";
+
 static const char *colors[][3] = {
 
     /*               fg         bg         border   */
