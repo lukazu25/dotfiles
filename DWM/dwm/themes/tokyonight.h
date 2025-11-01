9f63c14cc214fe693a597b3b33c2bb02f85a0db7
lukazu25 <145780979+lukazu25@users.noreply.github.com>
2025-11-01 13:06:17 +0400

    Update tokyonight.h

--- DWM/dwm/themes/tokyonight.h
+++ DWM/dwm/themes/tokyonight.h
@@ -1,21 +1,13 @@
 /* TokyoNight colors */
-static const char col_bg[]     = "#1a1b26";  // background
-static const char col_fg[]     = "#a9b1d6";  // foreground
-static const char col_blk[]    = "#32344a";  // black (normal)
-static const char col_red[]    = "#f7768e";  // red
-static const char col_grn[]    = "#9ece6a";  // green
-static const char col_ylw[]    = "#e0af68";  // yellow
-static const char col_blu[]    = "#7aa2f7";  // blue
-static const char col_mag[]    = "#ad8ee6";  // magenta
-static const char col_cyn[]    = "#0db9d7";  // cyan (highlight)
-static const char col_brblk[]  = "#444b6a";  // bright black
-
+static const char col_gray1[] = "#191425";
+static const char col_gray2[] = "#49415f";
+static const char col_gray3[] = "#c0c0c0";
+static const char col_gray4[] = "#f0f0f0";
+static const char col_accent[] = "#a0a0aa";
 static const char *colors[][3] = {
-    /*               fg       bg       border */
-    [SchemeNorm] = { col_fg,   col_bg,   col_brblk },
-    [SchemeSel]  = { col_cyn,  col_bg,   col_cyn   },
-    [SchemeHid]  = { col_cyn,  col_bg,   col_cyn  },
-};
 
-/* For tokyonight use this dmenu */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_cyn, "-sf", col_bg, NULL };
+    /*               fg         bg         border   */
+    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
+    [SchemeSel] = {col_gray4, col_gray2, col_accent},
+    [SchemeHid] = {col_accent, col_gray1, col_accent},
+};
